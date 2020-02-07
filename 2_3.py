dd32007bd21aaeb810294d2a02f8df57a7b67f55
ruiting-chen <51133017+ruiting-chen@users.noreply.github.com>
2020-02-07 08:53:47 -0600

    Create 2_3.py
    Adding a header and uploading to Github

---
+++ 2_3.py
@@ -0,0 +1,10 @@
+# Course: CS 30
+# Period: 1
+# Date created: 20/02/05
+# Date last modified: 20/02/07
+# Name: Ruiting Chen
+# Description: Printing name in a sentence
+
+
+name = "Ruiting"
+print("Hello " + name + ", would you like to learn some Python today?")
